63fabf2560140d3beeb47126380110ea1cc87f52
Raj Gupta <91501288+rajgupta007@users.noreply.github.com>
2024-01-02 22:16:58 +0530

    Update StringMethods.js

--- JavaScript/StringMethods.js
+++ JavaScript/StringMethods.js
@@ -1,1 +1,19 @@
-// in this we will see string methods
+// String Methods
+
+let animal = "Octopus";
+// reference at
+console.log(animal);
+console.log(animal[0]);
+
+console.log(animal[2]);
+
+console.log(animal[6]);
+
+console.log(animal[8]);
+
+//length
+console.log(animal.length);  // 7
+animal = animal + animal;
+console.log(animal);
+
+animal = "Octopus";
