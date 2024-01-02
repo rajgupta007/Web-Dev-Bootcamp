3549e7e7fc92eedf1a29670c73d1c84a0b925c92
Raj Gupta <91501288+rajgupta007@users.noreply.github.com>
2024-01-02 22:07:25 +0530

    Update Strings.js

--- JavaScript/Strings.js
+++ JavaScript/Strings.js
@@ -1,4 +1,10 @@
 // String.js in this we will see all about string 
 // stringf are immutable in JS
 // String is array of characters but in itself is an Object
-// 
+let name = 'KingPolo'
+
+name = 'this is my name'
+
+let newName = "This is new Name : 'RAJA Prasad'";
+
+
