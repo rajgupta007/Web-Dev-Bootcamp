b932c1ca712a614505e73f980f57158decb22ede
Raj Gupta <91501288+rajgupta007@users.noreply.github.com>
2024-01-02 21:35:07 +0530

    Add files via upload

---
+++ JavaScript/main.js
@@ -0,0 +1,38 @@
+// This is a comment line
+/*
+    This is multi line comment
+*/
+
+// Data Types in JS
+// Primitive Types
+
+// Number
+// String
+// Boolean
+// Null    
+// Undefined
+
+// Technically there are other two  1. Symbol and 2. BigInt
+// Basic Math operations
+var n1 = 10 , n2 = 3;
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
+console.log(Math.floor(n1 / n2));
+
+console.log(n1 ** n2);
+console.log(n1 % n2);
+
+// NaN is a numeric value that represents something that is not a number
+// 0/0      1+ NaN
+
+console.log(typeof 4);              // number
+console.log(typeof 0/0);            // NaN
+console.log(typeof NaN);            // number
+
+
+
+
+
+
